747d4760699786d8b29a0345f208c2a9307b3c63
AIDA9898 <138362134+Eliza2105@users.noreply.github.com>
2023-10-14 15:49:54 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// function calculateAge(year){
+//     return 2023-year
+// }
+
+
+// console.log(calculateAge(1998))
+// console.log(calculateAge(1980))
+
+// function information(name,year){
+//     const age = calculateAge(year)
+//     console.log('Бул адамдын аты:'+''+name+'Бул адамдын жашы)
+// }
+
+
+infoAbout('Argen',1989)
+
+let object = new object()
+let object ={}
+
+let user1= {
+    key:'value',
+    name: 'Argen'
+    age:27,
+    password:'asd123'
+}
+let user2 = {
+    key:'value',
+    name: 'Argen'
+    age:27,
+    password:'asd123'
+}
+console.log(user.name)
+console.log(Object.keys(user).length)
+console.log(Object.keys(user)[3])
+console.log(Object.values(user))
+
+let is_equal = user1==user2
+console.log(is_equal)
+
+let car = {
+    color:"red"
+    топлива:"бензин"
+    марка:"фуди",
+    максск:220
+}
